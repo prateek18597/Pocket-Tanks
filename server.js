432a9a4563c99f59aa1389ffda94b1fe939ce4e2
Pratik Parmar <prateek18597@gmail.com>
2018-05-07 14:15:05 +0530

    Terrain Coordinate

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
 
 // Routing
-app.get('/', function(request, response) {
+app.get('/Game', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
-  getLogin();
+  // getLogin();
 });
 
 // Starts the server.
@@ -39,8 +39,74 @@ var connection=mysql.createConnection(
 
 var password1="";
 var userid1="";
+var terr=[];//It contains Location of Terrain coordinates(X coordinate is index of array element,y coordinate is value of array)
+var xpeak=0;//It contains x coordinate of peak;
+var ypeak=400;//It contains y coordinate of peak;
 // var password2="";
 // var userid2="";
+
+function createTerrain()
+{
+	var ranX=Math.random()*400+300;
+	terr.push(400);
+	var y0=400;
+	// var xpeak=0;
+	// var ypeak=400;
+	for(var i=1;i<=ranX;i+=1)
+	{
+		var r=Math.random()*8/3;
+		var y=400;
+		var j=r%2;
+
+		if(ranX-i<200 && j>1)
+			r+=3;
+		if(j>1)
+		{
+			if(y-r>50)
+			{
+				y=y-r;
+				terr.push(y);
+			}	
+		}
+		else
+		{
+			if(y+r<400)
+			{
+				y=y+r;
+				terr.push(y);
+			}
+		}
+		xpeak=i;
+		ypeak=y;
+	}
+	for(var i=ranX+1;i<=1000;i+=1)
+	{
+		var r=Math.random()*8/3;
+		var y=400;
+		var j=r%2;
+
+		if(i-ranX<200 && j>1)
+			r+=3;
+		if(j>1)
+		{
+			if(y+r<400)
+			{
+				y=y+r;
+				terr.push(y);
+			}	
+		}
+		else
+		{
+			if(y-r>50)
+			{
+				y=y-r;
+				terr.push(y);
+			}
+		}
+	}
+}
+
+createTerrain();
 
 function getLogin()
 {
@@ -57,13 +123,22 @@ function getLogin()
 });
 }
 
+// connection.connect(function(err) {
+//   	if (err) throw err;
+
+//   	var sql="INSERT INTO Login (id,password) VALUES ('Hello','Bye')";
+//   		console.log("Second");
+//   		connection.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Data Inserted.");
+//   });
+// });	
+
 function setLogin(userid,pswd)
 {
 	console.log("SetLogin is working.");
 	connection.connect(function(err) {
-  	if (err) throw err;
-
-  	var sql="INSERT INTO Login (id,password) VALUES ("+String(userid)+","+String(pswd)+")";
+  	var sql="INSERT INTO Login (id,password) VALUES ('"+String(userid)+"','"+String(pswd)+"')";
   		console.log("Second");
   		connection.query(sql, function (err, result) {
     if (err) throw err;
@@ -78,7 +153,7 @@ io.on('connection', function(socket) {
   socket.on('bpress', function(data) {
     
  	socket.broadcast.emit('bpress2', data);
- 	
+	// setLogin("P","P"); 	
 }
 );
 
